f94723d8ace31142006b91bd5e25fb49a32b9e4f
hosseinx <93420888+hosseinx@users.noreply.github.com>
2025-08-17 23:25:57 +0330

    Update RLWM task.js

--- RLWM task.js
+++ RLWM task.js
@@ -1794,7 +1794,7 @@ function Thank_youRoutineBegin(snapshot) {
     // disable downloading results to browser
     psychoJS._saveResults = 0;
     //generate filename for results
-    let filename = psychoJS._experiment._experimentName + '_' + psychoJS._experiment._datetime + '.xlsx';
+    let filename = psychoJS._experiment._experimentName + '_' + psychoJS._experiment._datetime + '.csv';
     //extract data object from experiment
     let dataObj = psychoJS._experiment._trialsData;
     
@@ -1924,6 +1924,7 @@ async function quitPsychoJS(message, isCompleted) {
   
   return Scheduler.Event.QUIT;
 }
+
 
 
 
